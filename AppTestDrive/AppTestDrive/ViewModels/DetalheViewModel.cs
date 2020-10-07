807a99c637f63d76c94273bcc14ba55be11b30be
Jefferson Ferreira da Piedade <jfdapiedade@gmail.com>
2020-10-07 18:06:14 -0300

    Incluida a classe DetalheViewModel, desacoplando o código com o CodeBehind.

---
+++ AppTestDrive/AppTestDrive/ViewModels/DetalheViewModel.cs
@@ -0,0 +1,91 @@
+﻿using AppTestDrive.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AppTestDrive.ViewModels
+{
+    public class DetalheViewModel : INotifyPropertyChanged
+    {
+        public Veiculo Veiculo { get; set; }
+
+        public string DescricaoFreio
+        {
+            get { return String.Format("Freio Abs - R$ {0}", Veiculo.valorFreio); }
+        }
+        public string DescricaoArCondicionado
+        {
+            get { return String.Format("Ar Condicionado - R$ {0}", Veiculo.valorArCondicionado); }
+        }
+        public string DescricaoMP3
+        {
+            get { return String.Format("MP3 Player - R$ {0}", Veiculo.valorMP3); }
+        }
+
+
+        public bool FreioLigado
+        {
+            get
+            {
+                return Veiculo.FreioLigado;
+            }
+            set
+            {
+                Veiculo.FreioLigado = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        public bool ArCondicionadoLigado
+        {
+            get
+            {
+                return Veiculo.ArCondicionadoLigado;
+            }
+            set
+            {
+                Veiculo.ArCondicionadoLigado = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        public bool MP3Ligado
+        {
+            get
+            {
+                return Veiculo.MP3Ligado;
+            }
+            set
+            {
+                Veiculo.MP3Ligado = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        public string ValorTotal
+        {
+            get
+            {
+                return Veiculo.ValorTotalFormatado;
+            }
+        }
+
+        public DetalheViewModel(Veiculo veiculo)
+        {
+            Veiculo = veiculo;
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
--- AppTestDrive/AppTestDrive/Views/DetalheView.xaml.cs
+++ AppTestDrive/AppTestDrive/Views/DetalheView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using AppTestDrive.Models;
+using AppTestDrive.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,74 +15,10 @@ namespace AppTestDrive.Views
     {
         public Veiculo Veiculo { get; set; }
 
-        public string DescricaoFreio 
-        {
-            get { return String.Format("Freio Abs - R$ {0}", Veiculo.valorFreio); }
-        }
-        public string DescricaoArCondicionado 
-        { 
-            get { return String.Format("Ar Condicionado - R$ {0}", Veiculo.valorArCondicionado); } 
-        }
-        public string DescricaoMP3 
-        {
-            get { return String.Format("MP3 Player - R$ {0}", Veiculo.valorMP3); } 
-        }
-
-        
-        public bool FreioLigado { 
-            get 
-            {
-                return Veiculo.FreioLigado;
-            } 
-            set 
-            {
-                Veiculo.FreioLigado = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(ValorTotal));
-            } 
-        }
-
-        public bool ArCondicionadoLigado
-        {
-            get
-            {
-                return Veiculo.ArCondicionadoLigado;
-            }
-            set
-            {
-                Veiculo.ArCondicionadoLigado = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(ValorTotal));
-            }
-        }
-
-        public bool MP3Ligado
-        {
-            get
-            {
-                return Veiculo.MP3Ligado;
-            }
-            set
-            {
-                Veiculo.MP3Ligado = value;
-                OnPropertyChanged();
-                OnPropertyChanged(nameof(ValorTotal));
-            }
-        }
-
-        public string ValorTotal
-        {
-            get
-            {
-                return Veiculo.ValorTotalFormatado;
-            }
-        }
-
         public DetalheView(Veiculo veiculo)
         {
             InitializeComponent();
-            Veiculo = veiculo;
-            BindingContext = this;
+            BindingContext = new DetalheViewModel(veiculo);
         }
 
         private void BtnProximo_Clicked(object sender, EventArgs e)
